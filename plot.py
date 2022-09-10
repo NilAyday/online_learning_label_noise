a5a826b62a0d9b6b65baca68322ee4b246f7bbfd
NilAyday <73837355+NilAyday@users.noreply.github.com>
2022-09-10 14:04:50 +0200

    Update plot.py

--- plot.py
+++ plot.py
@@ -12,9 +12,9 @@ with open(file, 'rb') as handle:
     s = pickle.load(handle)
 
 x=range(len(s['train_acc']))
-plt.plot(x, np.ones(len(s['train_acc']))-s['train_acc'], label="Train Error",color="darkorange")
-plt.plot(x, np.ones(len(s['train_acc']))-s['true_train_acc'], label="Train Error wrt True Labels",color="royalblue")
-plt.plot(x, np.ones(len(s['val_acc']))-s['val_acc'], label="Test Error",color="lightgreen")
+plt.plot(x+50, np.ones(len(s['train_acc'])-50)-s['train_acc'][50:], label="Train Error",color="darkorange")
+plt.plot(x+50, np.ones(len(s['train_acc'])-50)-s['true_train_acc'][50:], label="Train Error wrt True Labels",color="royalblue")
+plt.plot(x+50, np.ones(len(s['val_acc'])-50)-s['val_acc'][50:], label="Test Error",color="lightgreen")
 plt.ylabel("Classification error")
 plt.xlabel("Number of samples")
 plt.legend()
