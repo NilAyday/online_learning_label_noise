5c8d78dd87e650abd0a6fce24c9a78aabf36df83
NilAyday <73837355+NilAyday@users.noreply.github.com>
2022-09-14 18:53:43 +0200

    Update sample_data.py

--- sample_data.py
+++ sample_data.py
@@ -17,7 +17,7 @@ indices_test = [i for i in range(len(ds_test))]
 random.shuffle(indices_test)
 #indices_train = [i for i in range(len(ds_train))]
 #random.shuffle(indices_train)
-batch_size=1
+batch_size=32
 
 num_data=len(ds_train.targets)
 num_data=300
@@ -30,7 +30,7 @@ dataloader_test = torch.utils.data.DataLoader(dataset_test, batch_size=1, shuffl
 
 dl_batch=[dataloader_train,dataloader_test]
 
-file= os.path.join(os.path.join(os.path.dirname(__file__)), './data/dataloader_single_CIFAR10.pickle')
+file= os.path.join(os.path.join(os.path.dirname(__file__)), './data/dataloader_CIFAR10.pickle')
 with open(file, 'wb') as handle:
     pickle.dump(dl_batch, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
