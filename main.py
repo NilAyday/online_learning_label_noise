916e8c8d3a8d5ccfba2d7d012edaccad24b1d6c4
NilAyday <73837355+NilAyday@users.noreply.github.com>
2022-09-14 20:56:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import pickle
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 import time
 
-num_iter=45000
+num_iter=1
 lr=0.001
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -20,14 +20,14 @@ model = Net()
 optimizer = torch.optim.SGD(model.parameters(), lr = lr)
 loss = torch.nn.CrossEntropyLoss()
 
-file= os.path.join(os.path.join(os.path.dirname(__file__)), './data/dataloader_batch.pickle')
+file= os.path.join(os.path.join(os.path.dirname(__file__)), './data/dataloader_single.pickle')
 with open(file, 'rb') as handle:
     [dataloader_train,dataloader_test] = pickle.load(handle)
 
 
 
 t0 = time.time()
-history=training.train(model, optimizer, loss, dataloader_train, dataloader_test, num_iter, device=device)
+history=training_online.train(model, optimizer, loss, dataloader_train, dataloader_test, num_iter, device=device)
 print("Training time:", time.time()-t0)
 print(history['true_train_acc'][-1])
 
