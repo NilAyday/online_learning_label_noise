637b05a5bfaddef4ec0b55b28abe10425743600e
NilAyday <73837355+NilAyday@users.noreply.github.com>
2022-09-15 11:29:48 +0200

    Update sample_data.py

--- sample_data.py
+++ sample_data.py
@@ -9,8 +9,8 @@ os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 import matplotlib.pyplot as plt
 
 # Takes the classes 0 airplane, 1 automobile and 8 ship
-ds_train=subloader.SubLoader(include_list=[0,1],root="./datasets", train=True, download=True, transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor()]))
-ds_test=subloader.SubLoader(include_list=[0,1],root="./datasets", train=False, download=True, transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor()]))
+ds_train=subloader.SubLoader(include_list=[0,1,2],root="./datasets", train=True, download=True, transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor()]))
+ds_test=subloader.SubLoader(include_list=[0,1,2],root="./datasets", train=False, download=True, transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor()]))
 
 
 indices_test = [i for i in range(len(ds_test))]
@@ -19,7 +19,9 @@ random.shuffle(indices_test)
 batch_size=1
 
 num_data=len(ds_train.targets)
-
+print(num_data)
+print(np.count_nonzero(ds_train.targets)))
+print(np.count_nonzero(ds_train.targets==1))
 #dataset_train = torch.utils.data.Subset(ds_train, indices_train[:])
 dataset_train = perturbed_dataloader.PerturbedDataset(ds_train, 0.3, size = num_data,num_classes = 2,enforce_false = False)
 dataloader_train = torch.utils.data.DataLoader(dataset_train, batch_size=batch_size, shuffle=True)
