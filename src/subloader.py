82dbf62ada73a51062a1b8db40b7a47ca3d0c3d7
ayday <ayday@fortiss.org>
2022-09-15 11:25:18 +0200

    changed subloader

--- src/subloader.py
+++ src/subloader.py
@@ -1,24 +1,40 @@
 import torchvision
 import numpy as np
+import random
 
 class SubLoader(torchvision.datasets.CIFAR10):
-    def __init__(self, *args, exclude_list=[], **kwargs):
+    def __init__(self, *args, include_list=[], num_data=50,**kwargs):
         super(SubLoader, self).__init__(*args, **kwargs)
 
-        if exclude_list == []:
+        if include_list == []:
             return
 
-        if self.train:
+        data=[]
+        targets=[]
+        for i in include_list:
+
+            exclude_list=np.delete(np.arange(10),i)
             labels = np.array(self.targets)
             exclude = np.array(exclude_list).reshape(1, -1)
             mask = ~(labels.reshape(-1, 1) == exclude).any(axis=1)
 
-            self.data = self.data[mask]
-            self.targets = labels[mask].tolist()
-        else:
-            labels = np.array(self.targets)
-            exclude = np.array(exclude_list).reshape(1, -1)
-            mask = ~(labels.reshape(-1, 1) == exclude).any(axis=1)
+            data.extend(self.data[mask][:num_data])
+            targets.extend(labels[mask].tolist()[:num_data])
 
-            self.data = self.data[mask]
-            self.targets = labels[mask].tolist()
+        self.data=data
+        self.targets=targets
+
+        '''
+
+        #i=0
+        exclude_list=[1,2,3,4,5,6,7,8,9,10]
+        labels = np.array(self.targets)
+        exclude = np.array(exclude_list).reshape(1, -1)
+        mask = ~(labels.reshape(-1, 1) == exclude).any(axis=1)
+
+        self.data_1 = self.data[mask][:num_data]
+        self.targets_1 = labels[mask].tolist()[:num_data]
+        
+        self.data=self.data_0 + self.data_1
+        self.targets=self.targets_0 + self.targets_1
+        '''
