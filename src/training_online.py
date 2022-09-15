995e5db371039aa4187e1ed91f21b934cb6ead46
NilAyday <73837355+NilAyday@users.noreply.github.com>
2022-09-15 09:59:41 +0200

    Update training_online.py

--- src/training_online.py
+++ src/training_online.py
@@ -43,6 +43,7 @@ def train(model, optimizer, loss_fn, train_dl, val_dl, epochs=100, device='cpu',
             x = batch[0].to(device)
             y = batch[1].to(device)
             yhat = model(x)
+            print(yhat)
             y_true = batch[2].to(device)
             
             '''
