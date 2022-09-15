67b2f686cb11a5f9d37efb398255fcbd6c3c7176
NilAyday <73837355+NilAyday@users.noreply.github.com>
2022-09-15 11:43:17 +0200

    Update sample_data.py

--- sample_data.py
+++ sample_data.py
@@ -10,8 +10,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Takes the classes 0 airplane, 1 automobile and 8 ship
-ds_train=subloader.SubLoader(include_list=[0,1,2],root="./datasets", train=True, download=True, transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor()]))
-ds_test=subloader.SubLoader(include_list=[0,1,2],root="./datasets", train=False, download=True, transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor()]))
+ds_train=subloader.SubLoader(include_list=[0,1,2],num_data=300,root="./datasets", train=True, download=True, transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor()]))
+ds_test=subloader.SubLoader(include_list=[0,1,2],num_data=60 ,root="./datasets", train=False, download=True, transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor()]))
 
 
 indices_test = [i for i in range(len(ds_test))]
@@ -27,8 +27,8 @@ print(np.count_nonzero(ds_train.targets==1))
 dataset_train = perturbed_dataloader.PerturbedDataset(ds_train, 0.3, size = num_data,num_classes = 3,enforce_false = False)
 dataloader_train = torch.utils.data.DataLoader(dataset_train, batch_size=batch_size, shuffle=True)
 
-dataset_test = torch.utils.data.Subset(ds_test, indices_test[:200])
-dataloader_test = torch.utils.data.DataLoader(dataset_test, batch_size=batch_size, shuffle=False)
+#dataset_test = torch.utils.data.Subset(ds_test, indices_test[:200])
+dataloader_test = torch.utils.data.DataLoader(ds_test, batch_size=batch_size, shuffle=False)
 
 dl_batch=[dataloader_train,dataloader_test]
 
